50cbfb60787018884d53b8997067fbaad093b089
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-04-11 22:24:55 -0500

    started incorporating commercejs products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,17 +4,17 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/u7khoqev6hy2xgsllrnb/revolution-5-road-running-shoes-szF7CS.png'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$5', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-silver-select-202011_GEO_US?wid=1280&hei=1190&fmt=png-alpha&.v=1632950226000'},
-]
+// const products = [
+//     {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/u7khoqev6hy2xgsllrnb/revolution-5-road-running-shoes-szF7CS.png'},
+//     {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$5', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-silver-select-202011_GEO_US?wid=1280&hei=1190&fmt=png-alpha&.v=1632950226000'},
+// ]
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
             <div className={classes.toolbar}/>
-            <Grid container justify='center' spacing-={4}>
+            <Grid container justifyContent='center' spacing-={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product}/>
